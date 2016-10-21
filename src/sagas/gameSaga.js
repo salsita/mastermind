4cd026642f1c6964c4a4cfa4b2eb282eea7c9074
Tomas Weiss <tomasw@salsitasoft.com>
2016-10-21 16:14:00 +0200

    starting a game workflow

--- src/sagas/gameSaga.js
+++ src/sagas/gameSaga.js
@@ -1,14 +1,29 @@
 import { call } from 'redux-saga/effects';
 
+import Backend from '../backend';
 import * as ConfirmationModalSaga from './confirmationModalSaga';
 
-export function* onBootstrap() {
-  const result = yield call(
-    ConfirmationModalSaga.showConfirmation,
-    'How are you?',
-    'Good',
-    'Better'
-  );
+export function* onStartGame() {
+  const gameInProgress = yield call(Backend.isGameInProgress);
 
-  console.log(result);
+  if (gameInProgress) {
+    // Ask user what they want to do:
+    // Start new game or Continue with the old one
+    const shouldStartNewGame = yield call(
+      ConfirmationModalSaga.showConfirmation,
+      'There\'s currently an active game in progress, do you want to start a new game?',
+      'Start New Game',
+      'Continue playing'
+    );
+
+    // If user wants to start a new game, we have to delete the active one
+    // and immediately after that, we can just a new one.
+    if (shouldStartNewGame) {
+      yield call(Backend.deleteActiveGame);
+      yield call(Backend.startGame);
+    }
+  } else {
+    // If there's no active game, just start one
+    yield call(Backend.startGame);
+  }
 }
